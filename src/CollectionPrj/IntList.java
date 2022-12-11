85465c5b35c1dc4de3cca2f2958b73fed0ab40fc
Dong1333 <mogijisung13@gmail.com>
2022-12-12 00:20:08 +0900

    정수형을 관리하는 컬랙션 만들어보기

--- src/CollectionPrj/IntList.java
+++ src/CollectionPrj/IntList.java
@@ -1,4 +1,41 @@
 package CollectionPrj;
 
+// 정수형을 관리하는 컬랙션 만들어보기
 public class IntList {
+
+    // 배열의 크기, 인덱스 번호를 의미하는 변수들 선언
+    // current == 현재위치
+    private int[] nums;
+    private int current;
+
+    // 생성자
+    public IntList(){
+        nums = new int[3];
+        current = 0;
+    }
+
+    // 정수형 배열에 num(값)을 하나씩 추가하고 현재위치 1증가
+    public void add(int num){
+        nums[current] = num;
+        current++;
+    }
+
+    // 현재위치를 강제적으로 0으로 되돌려서 다음에 들어올 값을 덮어쓰게 하기 위함
+    // 우선 직접 컬렉션을 구현하는 과정이니 이렇게 진행
+    public void clear(){
+        current = 0;
+    }
+    // 현재 사이즈를 반환하는 함수, 현재위치를 반환해도 된다.
+    public int size() {
+        return current;
+    }
+    // 배열에 있는 값을 가져오는 함수
+    // 3의 크기 배열에 7을 매개값으로 받게되는경우 예외 처리 진행
+    // 혹은 3을 매개 값으로 받아도 실제 배열은 (0,1,2) 이기 때문에 <= 를 사용
+    public int get (int index) {
+
+        if(current <= index)
+            throw new IndexOutOfBoundsException();
+        return nums[index];
+    }
 }
--- src/CollectionPrj/Program.java
+++ src/CollectionPrj/Program.java
@@ -1,4 +1,32 @@
 package CollectionPrj;
 
 public class Program {
+    public static void main(String[] args) {
+        // list 객체 생성, size, 현재 값 을 담을 변수 선언
+        IntList list = new IntList();
+        int size = 0;
+        int num = 0;
+
+        // list에 3, 5 추가 후 size값을 받고 출력
+        list.add(3);
+        list.add(5);
+        size = list.size();
+        System.out.printf("size : %d \n", size);
+
+        // 클리어
+        list.clear();
+
+        // 클리어 후 배열 사이즈 출력
+        size = list.size();
+        System.out.printf("size : %d \n", size);
+
+        // 7의 값 추가 후 배열의 0번째 값 받고 출력
+        list.add(7);
+        num = list.get(0);
+        System.out.printf("num :  %d \n", num);
+
+        // 현재 배열 1(2번째 위치)에 값이 없으므로 오류 출력
+        num = list.get(1);
+
+    }
 }
