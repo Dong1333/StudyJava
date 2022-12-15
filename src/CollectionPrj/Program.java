3f863699c43d6715f43c54f1841ce1a437f534a0
Dong1333 <mogijisung13@gmail.com>
2022-12-15 20:43:01 +0900

    ObjectList 클래스 생성 후 사용까지 완료 단점 : 어떠한 값을 담고 있는지, 값을 꺼낼때 warrper클래스 사용으로 인하여 코드 작업 증가 todo : 제네릭 사용해보기

--- src/CollectionPrj/Program.java
+++ src/CollectionPrj/Program.java
@@ -1,13 +1,16 @@
 package CollectionPrj;
 
 public class Program {
+
+    //  기존 IntList의 단점을 보완한 ObjectList를 사용해 보았다.
+    //  하지만 아직도 코드 수정의 번거움과, 어떠한 타입이 담겨있다는 정보 알기가 편리하진 않다..
+    //  이를 해결하고자 제네릭이란 것을 사용해야 한다
     public static void main(String[] args) {
-        // IntList 사용할 메인 코드
-        // list 객체 생성, size, 현재 값 을 담을 변수 선언
-        IntList list = new IntList();
+        // IntList,ObjectList  사용할 메인 코드
+        // list 객체 생성, list의 사이즈, list의 현재 값을 담을 변수 선언
+        ObjectList list = new ObjectList();
         int size = 0;
         int num = 0;
-
         // list에 3, 5 추가 후 size값을 받고 출력
         list.add(3);
         list.add(5);
@@ -23,11 +26,11 @@ public class Program {
 
         // 7의 값 추가 후 배열의 0번째 값 받고 출력
         list.add(7);
-        num = list.get(0);
+        num = (Integer) list.get(0);
         System.out.printf("num :  %d \n", num);
 
         // 현재 배열 1(2번째 위치)에 값이 없으므로 오류 출력
-        num = list.get(1);
+        // num = (Integer) list.get(1);
 
     }
 }
