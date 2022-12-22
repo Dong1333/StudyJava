fa3c93a7bc798c5c3d519370c87b88b193eab499
Dong1333 <mogijisung13@gmail.com>
2022-12-23 00:20:07 +0900

    상속, 인터페이스 개념 정리(오크 클래스 추가)

--- src/game/CharacterPac/Human.java
+++ src/game/CharacterPac/Human.java
@@ -2,7 +2,6 @@ package game.CharacterPac;
 
 // 사람 클래스
 public class Human extends Character {
-
     public Human(String name, int hp) {
         super(name, hp);
     }
--- src/game/CharacterPac/Orc.java
+++ src/game/CharacterPac/Orc.java
@@ -1,5 +1,6 @@
 package game.CharacterPac;
 
+// 캐릭터를 상속받는 (자식)오크 클래스
 public class Orc extends Character{
     public Orc(String name, int hp) {
         super(name, hp);
